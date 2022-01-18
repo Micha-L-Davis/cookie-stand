93eb1fbdd8b4fb82ad80fa0130192fbd3d1cd620
Micha Davis <m.leigh.davis@gmail.com>
2022-01-18 14:17:18 -0800

    refactored objects and functions to be more DRY

--- js/app.js
+++ js/app.js
@@ -1,118 +1,61 @@
 'use strict';
 
+//#region Global Document Reference
+
 let salesListSection = document.getElementById('sales-list');
 
-let seattle = {
-  location: 'Seattle',
-  minCust: 23,
-  maxCust: 65,
-  avgCookiePerSale: 6.3,
-  hourlySales: [],
-  randomHourlyCustomers: function() {
-    return randomIntInclusive(this.minCust, this.maxCust);
-  },
-  populateSalesData: function() {
-    let total = 0;
-    for (let i = 0; i < 13; i++) {
-      let sales = Math.floor(this.randomHourlyCustomers() * this.avgCookiePerSale);
-      total += sales;
-      let time = i + 6;
-      time = convertTo12Hr(time);
-      this.hourlySales[i] = `${time}: ${sales} cookies`;
-    }
-    this.hourlySales[13] = `Total: ${total} cookies`;
-  }
+//#endregion
+
+//#region Store Constructor and Prototyping
+
+let Store = function(location, minCust, maxCust, avgCookiePerSale) {
+  this.location = location;
+  this.maxCust = maxCust;
+  this.minCust = minCust;
+  this.avgCookiePerSale = avgCookiePerSale;
+  this.hourlySales = [];
+
+  Store.stores.push(this);
 };
 
-let tokyo = {
-  location: 'Tokyo',
-  minCust: 3,
-  maxCust: 24,
-  avgCookiePerSale: 1.2,
-  hourlySales: [],
-  randomHourlyCustomers: function() {
-    return randomIntInclusive(this.minCust, this.maxCust);
-  },
-  populateSalesData: function() {
-    let total = 0;
-    for (let i = 0; i < 13; i++) {
-      let sales = Math.floor(this.randomHourlyCustomers() * this.avgCookiePerSale);
-      total += sales;
-      let time = i + 6;
-      time = convertTo12Hr(time);
-      this.hourlySales[i] = `${time}: ${sales} cookies`;
-    }
-    this.hourlySales[13] = `Total: ${total} cookies`;
-  }
+Store.stores = [];
+
+Store.prototype.randomHourlyCustomers = function() {
+  return randomIntInclusive(this.minCust, this.maxCust);
 };
 
-let dubai = {
-  location: 'Dubai',
-  minCust: 11,
-  maxCust: 38,
-  avgCookiePerSale: 2.3,
-  hourlySales: [],
-  randomHourlyCustomers: function() {
-    return randomIntInclusive(this.minCust, this.maxCust);
-  },
-  populateSalesData: function() {
-    let total = 0;
-    for (let i = 0; i < 13; i++) {
-      let sales = Math.floor(this.randomHourlyCustomers() * this.avgCookiePerSale);
-      total += sales;
-      let time = i + 6;
-      time = convertTo12Hr(time);
-      this.hourlySales[i] = `${time}: ${sales} cookies`;
-    }
-    this.hourlySales[13] = `Total: ${total} cookies`;
+Store.prototype.populateSalesData = function() {
+  let total = 0;
+  for (let i = 0; i < 13; i++) {
+    let sales = Math.floor(this.randomHourlyCustomers() * this.avgCookiePerSale);
+    total += sales;
+    let time = i + 6;
+    time = convertTo12Hr(time);
+    this.hourlySales[i] = `${time}: ${sales} cookies`;
   }
+  this.hourlySales[13] = `Total: ${total} cookies`;
 };
 
-let paris = {
-  location: 'Paris',
-  minCust: 20,
-  maxCust: 38,
-  avgCookiePerSale: 2.3,
-  hourlySales: [],
-  randomHourlyCustomers: function() {
-    return randomIntInclusive(this.minCust, this.maxCust);
-  },
-  populateSalesData: function() {
-    let total = 0;
-    for (let i = 0; i < 13; i++) {
-      let sales = Math.floor(this.randomHourlyCustomers() * this.avgCookiePerSale);
-      total += sales;
-      let time = i + 6;
-      time = convertTo12Hr(time);
-      this.hourlySales[i] = `${time}: ${sales} cookies`;
-    }
-    this.hourlySales[13] = `Total: ${total} cookies`;
-  }
-};
+//#endregion
 
-let lima = {
-  location: 'Lima',
-  minCust: 2,
-  maxCust: 16,
-  avgCookiePerSale: 4.6,
-  hourlySales: [],
-  randomHourlyCustomers: function() {
-    return randomIntInclusive(this.minCust, this.maxCust);
-  },
-  populateSalesData: function() {
-    let total = 0;
-    for (let i = 0; i < 13; i++) {
-      let sales = Math.floor(this.randomHourlyCustomers() * this.avgCookiePerSale);
-      total += sales;
-      let time = i + 6;
-      time = convertTo12Hr(time);
-      this.hourlySales[i] = `${time}: ${sales} cookies`;
-    }
-    this.hourlySales[13] = `Total: ${total} cookies`;
-  }
-};
+//#region Creating stores and sales list
 
-let stores = [seattle, tokyo, dubai, paris, lima];
+// eslint-disable-next-line no-unused-vars
+const seattle = new Store('Seattle', 23, 65, 6.3);
+// eslint-disable-next-line no-unused-vars
+const tokyo = new Store('Tokyo', 3, 24, 1.2);
+// eslint-disable-next-line no-unused-vars
+const dubai = new Store('Dubai', 11, 38, 2.3);
+// eslint-disable-next-line no-unused-vars
+const paris = new Store('Paris', 20, 38, 2.3);
+// eslint-disable-next-line no-unused-vars
+const lima = new Store('Lima', 2, 16, 4.6);
+
+createStoreLists(Store.stores);
+
+//#endregion
+
+//#region Global Functions
 
 function randomIntInclusive(min, max) {
   return Math.floor(Math.random() * (max - min + 1) + min);
@@ -131,19 +74,21 @@ function convertTo12Hr(time) {
   return time;
 }
 
-function createSalesLists() {
+// Refactor this to have the for loop contents added as a method to the Store prototype
+function createStoreLists(stores) {
   for (let i = 0; i < stores.length; i++) {
-    createListHeader(stores[i]);
+    createListHeader(stores[i].location);
 
     const ulElem = createListBody();
 
-    populateSalesList(stores[i], ulElem);
+    stores[i].populateSalesData();
+    populateListData(stores[i].hourlySales, ulElem);
   }
 }
 
-function createListHeader(store) {
+function createListHeader(text) {
   const h3Elem = document.createElement('h3');
-  h3Elem.textContent = `${store.location}`;
+  h3Elem.textContent = `${text}`;
   salesListSection.appendChild(h3Elem);
   return h3Elem;
 }
@@ -154,13 +99,12 @@ function createListBody() {
   return ulElem;
 }
 
-function populateSalesList(store, listElement) {
-  store.populateSalesData();
-  for (let i = 0; i < store.hourlySales.length; i++) {
+function populateListData(arr, listElement) {
+  for (let i = 0; i < arr.length; i++) {
     const liElem = document.createElement('li');
-    liElem.textContent = `${store.hourlySales[i]}`;
+    liElem.textContent = `${arr[i]}`;
     listElement.appendChild(liElem);
   }
 }
 
-createSalesLists();
+//#endregion
